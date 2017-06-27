a377ebe3a729a1afe57ef6e34e9cee486b05fa06
leandroonline <leandro.rc.contato@gmail.com>
2017-06-27 10:31:37 -0300

    ADD sub category select annoucement

--- app/assets/javascripts/announcement/announcement.js
+++ app/assets/javascripts/announcement/announcement.js
@@ -1,12 +1,9 @@
 $(document).ready(function(){
-  // alert('carregado!');
   set_select_event();
 });
 
 function set_select_event(){
   $("#announcement_category_id").change(function(){
-    // alert('Selecionado!');
-
     var data = $("#announcement_category_id").val();
 
     $.ajax({
@@ -15,11 +12,29 @@ function set_select_event(){
             data: {"id": data},
             url: "/sub_categories/fetch",
             success: function(data){
-              for (var i in data) {
-                var texto = data[i]["id"] + " " + data[i]["sub_category_name"]
+              clearSelect(getSubCategorySelectElement());
+              addSubCategory(data);
              }
-            alert(texto);
-            }
-        });
+    });
   });
+}
+
+function clearSelect(select) {
+  while (select.options.length) {
+    select.remove(0);
+  }
+}
+
+function addSubCategory(data){
+  subCategorySelect = getSubCategorySelectElement();
+  for (var i in data) {
+    var option = document.createElement('option');
+    option.value = data[i].id;
+    option.innerHTML = data[i].sub_category_name;
+    subCategorySelect.appendChild(option);
+  }
+}
+
+function getSubCategorySelectElement() {
+  return document.getElementById("announcement_sub_category_id");
 }

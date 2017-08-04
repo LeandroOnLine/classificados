7406bab52d71a43ba73b49beafb9b9eb78e82cce
leandroonline <leandro.rc.contato@gmail.com>
2017-08-04 16:21:16 -0300

    ADD database default values

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def add_sub_category(category, list)
+  list.each do |name|
+    SubCategory.create(category_id: category.id, sub_category_name: name)
+  end
+end
+
+category = Category.create(category_name: 'Imóveis')
+  sub_category_list = ['Casas', 'Apartamentos', 'Kitnets', 'Lofts']
+  add_sub_category(category, sub_category_list)
+
+category = Category.create(category_name: 'Veículos')
+  sub_category_list = ['Carros', 'Motos']
+  add_sub_category(category, sub_category_list)
+
+category = Category.create(category_name: 'Animais')
+  sub_category_list = ['Cães', 'Gatos', 'Pássaros']
+  add_sub_category(category, sub_category_list)
